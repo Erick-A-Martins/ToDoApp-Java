ab4b055a4fc266bb0b129a9e44c6407f76582116
Erick-A-Martins <erickalvesqmartins@gmail.com>
2025-06-15 21:44:34 -0300

    refactor: change HandlerList to ContextHandlerCollection

--- src/main/java/com/erick/Main.java
+++ src/main/java/com/erick/Main.java
@@ -7,8 +7,6 @@ import org.eclipse.jetty.server.Server;
 import org.eclipse.jetty.server.handler.ContextHandlerCollection;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 import org.eclipse.jetty.servlet.ServletHolder;
-import org.eclipse.jetty.server.handler.HandlerList;
-import org.eclipse.jetty.server.Handler;
 
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
 import org.springframework.web.servlet.DispatcherServlet;
